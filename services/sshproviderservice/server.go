bc62dca61f2d5fd466f1f263eaa3ffbd83b64718
Scar26 <mmatty26@gmail.com>
2021-06-14 22:31:43 +0530

    feat(services/sshproviderservice): implement ssh session handler
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- services/sshproviderservice/server.go
+++ services/sshproviderservice/server.go
@@ -1,12 +1,19 @@
 package sshproviderservice
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/gliderlabs/ssh"
 	g "github.com/sdslabs/katana/configs"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 )
 
 var (
@@ -15,10 +22,61 @@ var (
 	kubeclient   *kubernetes.Clientset
 )
 
-func Server() {
-	ssh.Handle(func(s ssh.Session) {
-		s.Write([]byte(fmt.Sprintf("hello %s\n", s.User())))
+func ExecCmdExample(s ssh.Session) {
+
+	pathToCfg := filepath.Join(
+		os.Getenv("HOME"), ".kube", "config",
+	)
+
+	config, err := clientcmd.BuildConfigFromFlags("", pathToCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := []string{
+		"/bin/bash",
+	}
+
+	pods, err := client.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
+	podName := pods.Items[0].Name
+
+	req := client.CoreV1().RESTClient().Post().Resource("pods").Name(podName).
+		Namespace("default").SubResource("exec")
+	option := &v1.PodExecOptions{
+		Command: cmd,
+		Stdin:   true,
+		Stdout:  true,
+		Stderr:  true,
+		TTY:     true,
+	}
+
+	req.VersionedParams(
+		option,
+		scheme.ParameterCodec,
+	)
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  s,
+		Stdout: s,
+		Stderr: s,
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(err)
+}
+
+func Server() {
+	ssh.Handle(ExecCmdExample)
 	log.Println("starting ssh server on port 2222")
 	log.Fatal(ssh.ListenAndServe(":2222", nil))
 }
