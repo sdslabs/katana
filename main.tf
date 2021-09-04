cfa9f32a739d0990a1df3295c697f10c8c2e0046
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2021-09-04 22:30:36 +0530

    Added azure terraform file

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers = {
+      azurerm = {
+          source = "hashicorp/azurerm"
+          version = "~> 2.75.0"
+      }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "katana"
+  location = "india"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "katanaCluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
