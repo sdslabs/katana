d3990a5806550720e231e7f74771d40359837ada
mayankmittal-iitr <mmittal123456@gmail.com>
2021-12-07 20:18:19 +0530

    resolve merge conflict

--- types/mongo.go
+++ types/mongo.go
@@ -8,11 +8,12 @@ type AdminUser struct {
 }
 
 type CTFTeam struct {
-	Index    int    `json:"id" bson:"password" binding:"required"`
-	Name     string `json:"name" bson:"username" binding:"required"`
-	PodName  string `json:"podname" bson:"podname" binding:"required"`
-	Password string `json:"password" bson:"password" binding:"required"`
-	Score    int    `json:"score" bson:"score" binding:"required"`
+	Index     int    `json:"id" bson:"password" binding:"required"`
+	Name      string `json:"name" bson:"username" binding:"required"`
+	PodName   string `json:"podname" bson:"podname" binding:"required"`
+	Password  string `json:"password" bson:"password" binding:"required"`
+	PublicKey string `json:"publicKey" bson:"publicKey" binding:"required"` // TODO : initialize
+	Score     int    `json:"score" bson:"score" binding:"required"`
 }
 
 type Challenge struct {
