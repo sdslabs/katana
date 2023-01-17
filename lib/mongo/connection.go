1d2db3432073aa04079bfa901e8289b1d79a4903
h4shk4t <cyberboy9971@gmail.com>
2023-01-17 00:12:24 -0500

    Use ChallengeDeployerHost config URL

--- lib/mongo/connection.go
+++ lib/mongo/connection.go
@@ -14,7 +14,7 @@ import (
 )
 
 var ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-var client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+configs.MongoConfig.Username+":"+configs.MongoConfig.Password+"@"+utils.GetMongoIP()+":"+configs.MongoConfig.Port+"/?directConnection=true&appName=mongosh+"+configs.MongoConfig.Version))
+var client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+configs.MongoConfig.Username+":"+configs.MongoConfig.Password+"@"+configs.ServicesConfig.ChallengeDeployer.Host+":"+configs.MongoConfig.Port+"/?directConnection=true&appName=mongosh+"+configs.MongoConfig.Version))
 var link = client.Database(projectDatabase)
 
 func setupAdmin() {
