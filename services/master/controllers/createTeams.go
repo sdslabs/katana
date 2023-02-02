ab31147060df1cca77e35bd4b4ac08ad1149b62a
h4shk4t <cyberboy9971@gmail.com>
2023-02-02 07:32:04 +0530

    Rewrite team pod creation for different namespaces

--- services/master/controllers/createTeams.go
+++ services/master/controllers/createTeams.go
@@ -6,14 +6,75 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sdslabs/katana/lib/utils"
-	"github.com/sdslabs/katana/types"
 	coreV1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CreateTeam() types.CTFTeam {
-	ctfTeam := new(types.CTFTeam)
-	return *ctfTeam
+func CreateTeam(namespace string, i string) *coreV1.Pod {
+	// ctfTeam := new(types.CTFTeam)
+	log.Println("Creating pod for team " + i)
+	teamPod := &coreV1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "katana-team-" + i,
+			Namespace: namespace,
+		},
+		Spec: coreV1.PodSpec{
+			Containers: []coreV1.Container{
+				{
+					Name:  "teamvm",
+					Image: "scar26/sdskatanad",
+					Env: []coreV1.EnvVar{
+						{
+							Name: "CHALLENGE_DIR",
+							ValueFrom: &coreV1.EnvVarSource{
+								ConfigMapKeyRef: &coreV1.ConfigMapKeySelector{
+									LocalObjectReference: coreV1.LocalObjectReference{
+										Name: "teamvm-config",
+									},
+									Key: "challenge_dir",
+								},
+							},
+						},
+						{
+							Name: "TMP_DIR",
+							ValueFrom: &coreV1.EnvVarSource{
+								ConfigMapKeyRef: &coreV1.ConfigMapKeySelector{
+									LocalObjectReference: coreV1.LocalObjectReference{
+										Name: "teamvm-config",
+									},
+									Key: "tmp_dir",
+								},
+							},
+						},
+						{
+							Name: "INIT_FILE",
+							ValueFrom: &coreV1.EnvVarSource{
+								ConfigMapKeyRef: &coreV1.ConfigMapKeySelector{
+									LocalObjectReference: coreV1.LocalObjectReference{
+										Name: "teamvm-config",
+									},
+									Key: "init_file",
+								},
+							},
+						},
+						{
+							Name: "DAEMON_PORT",
+							ValueFrom: &coreV1.EnvVarSource{
+								ConfigMapKeyRef: &coreV1.ConfigMapKeySelector{
+									LocalObjectReference: coreV1.LocalObjectReference{
+										Name: "teamvm-config",
+									},
+									Key: "daemon_port",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return teamPod
 }
 
 func CreateTeams(c *fiber.Ctx) error {
@@ -31,11 +92,16 @@ func CreateTeams(c *fiber.Ctx) error {
 		log.Println("Creating Team: " + strconv.Itoa(i))
 		nsName := &coreV1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "ctf-team-" + strconv.Itoa(i),
+				Name: "katana-team-ns-" + strconv.Itoa(i),
 			},
 		}
 
 		_, err = client.CoreV1().Namespaces().Create(c.Context(), nsName, metav1.CreateOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		pod := CreateTeam("katana-team-ns-"+strconv.Itoa(i), strconv.Itoa(i))
+		_, err = client.CoreV1().Pods(pod.Namespace).Create(c.Context(), pod, metav1.CreateOptions{})
 		if err != nil {
 			log.Fatal(err)
 		}
