f2e838fbb4b033759ea9e42c29521851a96c1d9f
Anshul Singh <anshulspidy@gmail.com>
2023-02-17 21:57:22 +0530

    Docs Init

--- docs/content/getting-started/_index.md
+++
@@ -1,7 +0,0 @@
----
-title: "Getting Started"
-date: 2023-02-17T21:32:50+05:30
----
-
-### Chapter 1
-# Getting StartedBuild a documentation website for your service using Hugo and hugo-theme-learn in less than an hour.
--- docs/themes/hugo-geekdoc/src/js/katex.js
+++
@@ -1,9 +0,0 @@
-import "katex/dist/katex.css"
-
-document.addEventListener("DOMContentLoaded", function () {
-  import("katex/dist/contrib/auto-render")
-    .then(({ default: renderMathInElement }) => {
-      renderMathInElement(document.body)
-    })
-    .catch((error) => console.error(error))
-})
