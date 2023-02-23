b244553c4bc454ceccda6ba71805f7d1119ff3de
Ashish <ashishndiitr@gmail.com>
2023-02-24 01:17:00 +0530

    testing

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -2,24 +2,31 @@ package controllers
 
 import (
 	"fmt"
-	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func git(arg string) {
-	app := "git"
-	arg0 := arg
-	cmd := exec.Command(app, arg0)
-	stdout, err := cmd.Output()
+// func git(arg string) {
+// 	app := "git"
+// 	arg0 := arg
+// 	cmd := exec.Command(app, arg0)
+// 	stdout, err := cmd.Output()
 
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(string(stdout))
-}
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	fmt.Println(string(stdout))
+// }
 
+//	func Chall() string {
+//		cmd, err := exec.Command("/bin/sh", "asdf.sh").Output()
+//		if err != nil {
+//			fmt.Printf("error %s", err)
+//		}
+//		output := string(cmd)
+//		return output
+//	}
 func ChallengeUpdate(c *fiber.Ctx) error {
 	fmt.Println("Hello")
 	//TODO : Apply yaml file and up challenge in container in docker
