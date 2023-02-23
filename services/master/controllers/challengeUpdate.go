f34e49d50106ccb62ed49c9967e74f0b6423f593
Ashish <ashishndiitr@gmail.com>
2023-02-24 02:06:40 +0530

    team1

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -2,33 +2,20 @@ package controllers
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-// func git(arg string) {
-// 	app := "git"
-// 	arg0 := arg
-// 	cmd := exec.Command(app, arg0)
-// 	stdout, err := cmd.Output()
+func ChallenegUpdate(c *fiber.Ctx) error {
 
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return
-// 	}
-// 	fmt.Println(string(stdout))
-// }
+	cmd := exec.Command("/bin/sh", "../../../teams/team1/script.sh")
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// Print the output
+	fmt.Println(string(stdout))
 
-//	func Chall() string {
-//		cmd, err := exec.Command("/bin/sh", "asdf.sh").Output()
-//		if err != nil {
-//			fmt.Printf("error %s", err)
-//		}
-//		output := string(cmd)
-//		return output
-//	}
-func ChallengeUpdate(c *fiber.Ctx) error {
-	fmt.Println("Hello")
-	//TODO : Apply yaml file and up challenge in container in docker
 	return c.SendString("Challenge successfully pulled from gi")
 }
