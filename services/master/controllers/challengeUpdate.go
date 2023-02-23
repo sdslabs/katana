050aa9f1d6b00ec6cde361c849885607182d5375
Ashish <ashishndiitr@gmail.com>
2023-02-24 04:27:19 +0530

    Struct for body parser

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -2,20 +2,26 @@ package controllers
 
 import (
 	"fmt"
-	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ChallengeUpdate(c *fiber.Ctx) error {
 
-	cmd := exec.Command("/bin/sh", "../../../teams/team1/script.sh")
-	stdout, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	// Print the output
-	fmt.Println(string(stdout))
+	//cmd := exec.Command("/bin/sh", "../../../teams/team1/script.sh")
 
-	return c.SendString("Challenge successfully pulled from gi")
+	type Teams struct {
+		URL  int    `json:"url"`
+		Name string `json:"name"`
+	}
+	var req Teams
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
+	}
+
+	// Print out the ID value
+	fmt.Printf("Received : %d\n", req.URL)
+
+	return c.SendString(fmt.Sprintf("Received full_name: %s", req.Name))
+
 }
