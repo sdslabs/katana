5cbdc7a3536137bc018d1729dde6ccf220e9fc68
Ashish <ashishndiitr@gmail.com>
2023-02-24 04:45:39 +0530

    testing

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,18 +8,18 @@ func ChallengeUpdate(c *fiber.Ctx) error {
 
 	//cmd := exec.Command("/bin/sh", "../../../teams/team1/script.sh")
 
-	type Teams struct {
-		URL  int    `json:"url"`
-		Name string `json:"name"`
-	}
-	var req Teams
-	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
-	}
+	// type Teams struct {
+	// 	URL    int    `json:"url"`
+	// 	BEFORE string `json:"before"`
+	// }
+	// var req Teams
+	// if err := c.BodyParser(&req); err != nil {
+	// 	return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
+	// }
 
-	// Print out the ID value
-	fmt.Printf("Received : %d\n", req.URL)
+	// // Print out the ID value
+	// fmt.Printf("Received : %d\n", req.URL)
 
-	return c.SendString(fmt.Sprintf("Received full_name: %s", req.Name))
-
+	//return c.SendString(fmt.Sprintf("Received full_name: %s", req.BEFORE))
+	return c.SendString("Working")
 }
