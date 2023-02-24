eb71ed4155fcb8df992fa2405913d71798caf195
Vansh Uppal <vanshuppal2002@gmail.com>
2023-02-24 19:11:59 +0530

    ok

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -1,24 +1,33 @@
 package controllers
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func ChallengeUpdate(c *fiber.Ctx) error {
 
-	//cmd := exec.Command("/bin/sh", "../../../teams/team1/script.sh")
+	cmd := exec.Command("/bin/sh", "./teams/team1/script.sh")
 
-	// type Teams struct {
-	// 	URL    int    `json:"url"`
-	// 	BEFORE string `json:"before"`
-	// }
-	// var req Teams
-	// if err := c.BodyParser(&req); err != nil {
-	// 	return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
-	// }
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(stdout))
 
-	// // Print out the ID value
-	// fmt.Printf("Received : %d\n", req.URL)
+	type Teams struct {
+		URL    string `json:"url"`
+		BEFORE string `json:"before"`
+	}
+	var req Teams
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
+	}
+
+	// Print out the ID value
+	fmt.Printf("Received : %d\n", req)
 
 	//return c.SendString(fmt.Sprintf("Received full_name: %s", req.BEFORE))
 	return c.SendString("Working")
