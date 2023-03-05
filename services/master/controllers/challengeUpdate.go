edb04baff148e88fa40c5b66df14b61b9c6ae75b
Ashish <ashishndiitr@gmail.com>
2023-03-05 23:05:05 +0530

    minor changes

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -9,25 +9,27 @@ import (
 
 func ChallengeUpdate(c *fiber.Ctx) error {
 
-	cmd := exec.Command("/bin/sh", "./teams/team1/script.sh")
-
-	stdout, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(string(stdout))
-
 	type Teams struct {
-		URL    string `json:"url"`
-		BEFORE string `json:"before"`
+		TEAMNAME string `json:"url"`
+		BEFORE   string `json:"before"`
 	}
 	var req Teams
 	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
 	}
 
+	teamname := req.TEAMNAME
+
 	// Print out the ID value
-	fmt.Printf("Received : %d\n", req)
+	fmt.Printf("Received : %v\n", req.TEAMNAME)
+
+	cmd := exec.Command("/bin/sh", "./teams/"+teamname+"/script.sh")
+
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(stdout))
 
 	//return c.SendString(fmt.Sprintf("Received full_name: %s", req.BEFORE))
 	return c.SendString("Working")
