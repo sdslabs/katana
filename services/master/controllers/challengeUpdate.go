227a7fd0ab184ec0fae86729c0506584693a168b
Ashish <ashish@Beast-prod.3a1ktsmy5tvuzenlrx03dqoyvc.rx.internal.cloudapp.net>
2023-03-06 23:10:44 +0530

    pathing done

--- services/master/controllers/challengeUpdate.go
+++ services/master/controllers/challengeUpdate.go
@@ -3,35 +3,51 @@ package controllers
 import (
 	"fmt"
 	"os/exec"
-
 	"github.com/gofiber/fiber/v2"
 )
 
+
+type Repo struct {
+	Name string `json:"name"`
+}
+
+type Teams struct {
+	Ref  string `json:"ref"`
+	Before   string `json:"before"`
+	Repository Repo `json:"repository"`
+}
+
 func ChallengeUpdate(c *fiber.Ctx) error {
 
-	type Teams struct {
-		TEAMNAME string `json:"url"`
-		BEFORE   string `json:"before"`
-	}
-	var req Teams
-	if err := c.BodyParser(&req); err != nil {
+	p := new(Teams)
+	if err := c.BodyParser(p); err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("Failed to parse JSON request body: %v", err))
 	}
 
-	teamname := req.TEAMNAME
+	// fmt.Println(p.Repository)
+	// fmt.Println(p.Repository.Name)
+	teamname := p.Repository.Name
+	//fmt.Println(teamname)
+	
+	//for non-blocking the script run
+	ch := make(chan bool)
 
-	// Print out the ID value
-	fmt.Printf("Received : %v\n", req)
-	fmt.Printf("Test : %s\n", teamname)
+	go func(){
 
-	cmd := exec.Command("/bin/sh", "./teams/team1/script.sh")
+		cmd := exec.Command("/bin/sh", "./teams/"+teamname+"/script.sh")
+		stdout, err := cmd.Output()
+	        if err != nil {
+                	fmt.Println(err.Error())
+       		 }
+		fmt.Println(string(stdout))
 
-	stdout, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(string(stdout))
+		ch <- true
+	}()
 
-	//return c.SendString(fmt.Sprintf("Received full_name: %s", req.BEFORE))
+	return c.SendString(fmt.Sprintf("Received Payload"))
+	
+	<-ch
+
 	return c.SendString("Working")
+ 
 }
