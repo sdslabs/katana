6ec7fd5bcc9420d13f8c490645dcdcbe464ea616
Aditya Bisht <adityabisht64@gmail.com>
2023-03-08 05:33:12 +0530

    WIP: MySQL conn

---
+++ services/master/controllers/gitServer.go
@@ -0,0 +1,64 @@
+package controllers
+
+import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sdslabs/katana/configs"
+)
+
+func GitServer(c *fiber.Ctx) error {
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	mysqlAddress := configs.ServicesConfig.ChallengeDeployer.Host + ":" + configs.MySQLConfig.Port
+	gitServerAddress := "10.25.1.18"
+
+	// Add the form fields
+	writer.WriteField("db_type", "MySQL")
+	writer.WriteField("db_host", mysqlAddress)
+	writer.WriteField("db_user", configs.MySQLConfig.Username)
+	writer.WriteField("db_passwd", configs.MySQLConfig.Password)
+	writer.WriteField("db_name", "gogs")
+	writer.WriteField("db_schema", "public")
+	writer.WriteField("ssl_mode", "disable")
+	writer.WriteField("db_path", "/app/gogs/data/gogs.db")
+	writer.WriteField("app_name", "Gogs")
+	writer.WriteField("repo_root_path", "/data/git/gogs-repositories")
+	writer.WriteField("run_user", "git")
+	writer.WriteField("domain", gitServerAddress)
+	writer.WriteField("ssh_port", "22")
+	writer.WriteField("http_port", "3000")
+	writer.WriteField("app_url", "http://"+gitServerAddress+":3000/")
+	writer.WriteField("log_root_path", "/app/gogs/log")
+	writer.WriteField("default_branch", "master")
+
+	// Close the writer
+	writer.Close()
+
+	// Create the request
+	req, err := http.NewRequest("POST", "http://"+gitServerAddress+"/install", &requestBody)
+	if err != nil {
+		return err
+	}
+
+	// Set the content type
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error while setting up Git Server")
+	}
+
+	return c.SendString("Git Server setup completed\n")
+}
--- services/master/server.go
+++ services/master/server.go
@@ -40,6 +40,7 @@ func Server() error {
 	admin.Get("/createTeams/:number", c.CreateTeams)
 	admin.Post("/logs", c.Logs)
 	admin.Get("/deploy", c.Deploy)
+	admin.Get("/gitServer", c.GitServer)
 	admin.Get("/cluster/:id", c.ClusterInfo)
 	fmt.Printf("Listening on %s:%d\n", cfg.APIConfig.Host, cfg.APIConfig.Port)
 	return app.Listen(fmt.Sprintf("%s:%d", cfg.APIConfig.Host, cfg.APIConfig.Port))
