eff9dd1e5b8e0129de3678d20ba12b4514d82928
Aditya Bisht <adityabisht64@gmail.com>
2023-03-10 09:13:38 +0530

    Remove redundant code

--- services/master/controllers/infraSet.go
+++ services/master/controllers/infraSet.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,40 +12,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
-
-func sshkeyExists(path string) bool {
-	_, err := os.Stat(path)
-	return !os.IsNotExist(err)
-}
-
-func sshkeyCreate(path string) {
-	//f, err := os.Create("testFile.txt")
-
-	app := "ssh-keygen"
-	arg0 := "-t"
-	arg1 := "ed25519"
-	arg2 := "-C"
-	arg3 := "\"test@gmail.com\""
-	arg4 := "-f"
-	arg5 := path
-	arg6 := "-q"
-	arg7 := "-P"
-	arg8 := "\"\""
-
-	cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-
-	stdout, err := cmd.Output()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println(string(stdout))
-
-	//defer f.Close()
-
-}
 
 func InfraSet(c *fiber.Ctx) error {
 
@@ -75,16 +40,6 @@ func InfraSet(c *fiber.Ctx) error {
 	}
 
 	fmt.Println(kubeclient)
-
-	var filelocation string = "/tmp/ssh_admin"
-	exist := sshkeyExists(filelocation)
-
-	if exist {
-		fmt.Println("Admin ssh file exists")
-	} else {
-		fmt.Println("Creating ssh keys")
-		sshkeyCreate(filelocation)
-	}
 
 	return c.SendString("Infrastructure setup completed")
 }
