0b711964345afe907a3611ab142f6b9870674d71
ashpect <ashishndiitr@gmail.com>
2023-06-03 23:47:18 +0530

    challenge gets deployed

--- services/challengedeployerservice/server.go
+++ services/challengedeployerservice/server.go
@@ -76,58 +76,55 @@ func DeployToAll(localFilePath string, pathInPod string) error {
 	return nil
 }
 
-func DeployChallenge() error {
+func DeployChallenge(chall_name, team_name string) error {
+
+	// chall_name = "notekeeper"
+	// team_name = "team-0"
+	team_namespace := "katana-" + team_name + "-ns"
 
 	fmt.Println("TEST 1")
 	if err := GetClient(g.KatanaConfig.KubeConfig); err != nil {
 		return err
 	}
-	fmt.Println("TEST 2")
+	//fmt.Println("TEST 2")
 	//Change namespace
-	deploymentsClient := kubeclient.AppsV1().Deployments(v1.NamespaceDefault)
+	deploymentsClient := kubeclient.AppsV1().Deployments(team_namespace)
 
-	fmt.Println("TEST 3")
-	// // Read the deployment YAML file
-	// pwd, _ := os.Getwd()
-	// fmt.Println(pwd)
-	// deploymentYAML, err := ioutil.ReadFile("web_challenge.yaml")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	//fmt.Println("TEST 3")
+	//Get and print all namespaces for testing
+	namespaces, err := kubeclient.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(namespaces.Items); i++ {
+		fmt.Println(namespaces.Items[i].Name)
+	}
 
-	fmt.Println("TEST 3.5")
-	// // Open the deployment YAML file
-	// file, err := os.Open("web_challenge.yaml")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer file.Close()
-
-	fmt.Println("TEST 4")
-
-	// Create Deployment struct dynamically when we switch to various challenges other than web.
+	//fmt.Println("TEST 4")
+	// Creates Deployment object and deploys it
 	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "chall-notekeeper",
+			Namespace: team_namespace,
+			Name:      chall_name,
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: int32Ptr(2),
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": "chall-notekeeper",
+					"app": chall_name,
 				},
 			},
 			Template: v1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app": "chall-notekeeper",
+						"app": chall_name,
 					},
 				},
 				Spec: v1.PodSpec{
 					Containers: []v1.Container{
 						{
-							Name:            "chall-notekeeper-team-0",
-							Image:           "notekeeper:latest",
+							Name:            chall_name + "-" + team_name,
+							Image:           chall_name + ":latest",
 							ImagePullPolicy: v1.PullPolicy("Never"),
 							Ports: []v1.ContainerPort{
 								{
@@ -143,6 +140,38 @@ func DeployChallenge() error {
 		},
 	}
 
+	//fmt.Println("TEST 5")
+	// Create Deployment
+	fmt.Println("Creating deployment...")
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+
+	if err != nil {
+		fmt.Println(" FAT GYA..SADGE :( ")
+		panic(err)
+	}
+
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+	return nil
+
+	// Trying this method of deployment by reading the YAML file , parsing it and then creating the deployment
+	// The above method also works, but this can be explored when mulitple challenges type are added
+	// https://github.com/kubernetes/client-go/issues/193
+
+	// // Read the deployment YAML file
+	// pwd, _ := os.Getwd()
+	// fmt.Println(pwd)
+	// deploymentYAML, err := ioutil.ReadFile("web_challenge.yaml")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // Open the deployment YAML file
+	// file, err := os.Open("web_challenge.yaml")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
 	// decoder := yaml.NewYAMLOrJSONDecoder(file, 4096)
 	// err = decoder.Decode(deployment)
 	// if err != nil {
@@ -155,17 +184,6 @@ func DeployChallenge() error {
 	// 	panic(err)
 	// }
 
-	fmt.Println("TEST 6")
-	// Create Deployment
-	fmt.Println("Creating deployment...")
-	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("TEST 7")
-	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
-	return nil
 }
 
 func int32Ptr(i int32) *int32 { return &i }
