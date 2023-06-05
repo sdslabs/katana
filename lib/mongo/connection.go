1cc5376dcfeb0374e7959bf5e7e3786ec8127ee4
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2023-06-06 03:59:36 +0530

    finish bugs in patching

--- lib/mongo/connection.go
+++ lib/mongo/connection.go
@@ -37,7 +37,7 @@ func setupAdmin() {
 }
 
 func setup() {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	err = client.Ping(ctx, nil)
 	if err != nil {
