2a735bfe33306ccc1528e1c9eea224465e5628ac
h4shk4t <ashutosh3002@gmail.com>
2023-06-19 02:07:23 +0530

    Add challchecker cronjob specs

--- services/challengecheckerservice/deployChallCheck.go
+++ services/challengecheckerservice/deployChallCheck.go
@@ -3,12 +3,11 @@ package challengecheckerservice
 import (
 	"context"
 	"fmt"
-	"log"
+	"time"
 
 	g "github.com/sdslabs/katana/configs"
 	batchv1 "k8s.io/api/batch/v1"
-	batchv1beta "k8s.io/api/batch/v1beta1"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -18,27 +17,24 @@ func DeployChallChecker(chall_name, port, team_namespace string) error {
 	if err := GetClient(g.KatanaConfig.KubeConfig); err != nil {
 		return err
 	}
-
-	jobs := kubeclient.BatchV1beta1().CronJobs(namespace)
-
-	jobSpec := &batchv1beta.CronJob{
+	cronJobSpec := &batchv1.CronJob{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      chall_name + "-checker",
 			Namespace: namespace,
 		},
-		Spec: batchv1beta.CronJobSpec{
-			Schedule: "* */2 * * *",
-			JobTemplate: batchv1beta.JobTemplateSpec{
+		Spec: batchv1.CronJobSpec{
+			Schedule:          "*/1 * * * *", // Every minute.
+			ConcurrencyPolicy: batchv1.ForbidConcurrent,
+			JobTemplate: batchv1.JobTemplateSpec{
 				Spec: batchv1.JobSpec{
-					Template: v1.PodTemplateSpec{
-						Spec: v1.PodSpec{
-							Containers: []v1.Container{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
 								{
-
 									Name:            chall_name + "-checker",
 									Image:           chall_name + "-checker:latest",
-									ImagePullPolicy: v1.PullPolicy("Never"),
-									Env: []v1.EnvVar{
+									ImagePullPolicy: corev1.PullPolicy("Never"),
+									Env: []corev1.EnvVar{
 										{
 											Name:  "URL",
 											Value: "http://" + chall_name + "." + team_namespace + ".svc.cluster.local:" + port,
@@ -46,31 +42,42 @@ func DeployChallChecker(chall_name, port, team_namespace string) error {
 									},
 								},
 							},
-							RestartPolicy: v1.RestartPolicyNever,
+							RestartPolicy: corev1.RestartPolicyOnFailure,
 						},
 					},
 				},
 			},
 		},
 	}
-	cronJob, err := jobs.Get(context.TODO(), chall_name+"-checker", metav1.GetOptions{})
+
+	cronJob, _ := kubeclient.BatchV1().CronJobs(namespace).Get(context.TODO(), chall_name+"-checker", metav1.GetOptions{})
 	if cronJob.Name == chall_name+"-checker" {
 		fmt.Println("Challenge checker already exists for the challenge " + chall_name + " in namespace " + namespace)
 		return nil
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	_, err := kubeclient.BatchV1().CronJobs(namespace).Create(ctx, cronJobSpec, metav1.CreateOptions{})
 	if err != nil {
-		log.Println(err)
+		panic(err.Error())
 	}
 
-	fmt.Println("Creating challenge checker (CronJob) for the challenge " + chall_name + " in namespace " + namespace)
-	result, err := jobs.Create(context.Background(), jobSpec, metav1.CreateOptions{})
+	fmt.Println("Cron job created successfully!")
 
-	if err != nil {
-		fmt.Println("Failed to create challenge checker for the challenge " + chall_name + " in namespace " + namespace)
-		panic(err)
-	}
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	fmt.Printf("Created cronjob %q.\n", result.GetObjectMeta().GetName()+" in namespace "+namespace+"\n")
+	// fmt.Println("Creating challenge checker (CronJob) for the challenge " + chall_name + " in namespace " + namespace)
+	// result, err := jobs.Create(context.TODO(), jobSpec, metav1.CreateOptions{})
+
+	// if err != nil {
+	// 	fmt.Println("Failed to create challenge checker for the challenge " + chall_name + " in namespace " + namespace)
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Created cronjob %q.\n", result.GetObjectMeta().GetName()+" in namespace "+namespace+"\n")
 	return nil
 }
